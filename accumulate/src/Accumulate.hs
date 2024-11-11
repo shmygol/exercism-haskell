9b458adf17571b7690f994fdcc172201846006c8
Ilya Shmygol <ishmygol@gmail.com>
2024-11-11 19:50:01 +0100

    Solve exercise accumulate v1

--- accumulate/src/Accumulate.hs
+++ accumulate/src/Accumulate.hs
@@ -1,4 +1,10 @@
 module Accumulate (accumulate) where
 
+-- It's basically a map function and can be defined just like this:
+-- accumulate :: (a -> b) -> [a] -> [b]
+-- accumulate = map
+
+-- But we can also define it recursively:
 accumulate :: (a -> b) -> [a] -> [b]
-accumulate f xs = error "You need to implement this function."
+accumulate _ [] = []
+accumulate f (x:xs) = f x : accumulate f xs
