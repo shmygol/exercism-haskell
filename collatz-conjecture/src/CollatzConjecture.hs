91a7bbcaaf9ec29aaf618aba052a74bc0c99b6a0
Ilya Shmygol <ishmygol@gmail.com>
2024-11-12 14:19:20 +0100

    Solve exercise collatz-conjecture v1

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,9 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz n = error "You need to implement this function."
+collatz = recursiveCollatz 0
+  where
+    recursiveCollatz counter x
+      | x < 1 = Nothing
+      | x == 1 = Just counter
+      | otherwise = recursiveCollatz (counter + 1) (if even x then (x `div` 2) else (x * 3 + 1))
