3f335992917a8f08ec83773aea5a4cdf93340ecb
Ilya Shmygol <ishmygol@gmail.com>
2024-11-12 15:15:56 +0100

    Solve exercise rna-transcription v1

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -1,4 +1,15 @@
 module DNA (toRNA) where
 
+import qualified Data.Map as Map
+
+rules :: Map.Map Char Char
+rules = Map.fromList [('G', 'C'),
+                      ('C', 'G'),
+                      ('T', 'A'),
+                      ('A', 'U')]
+
+transcript :: Char -> Either Char Char
+transcript char = maybe (Left char) Right (Map.lookup char rules)
+
 toRNA :: String -> Either Char String
-toRNA xs = error "You need to implement this function."
+toRNA = traverse transcript
