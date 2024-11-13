7c0f6b5ea85849d3f6a441530626372f552b0e39
Ilya Shmygol <ishmygol@gmail.com>
2024-11-13 10:15:27 +0100

    Solve exercise sum-of-multiples v1

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,21 @@
 module SumOfMultiples (sumOfMultiples) where
 
+-- import Prelude
+-- import Data.Foldable (sum)
+-- import Data.Set as Set
+-- import Data.List.Lazy as List
+import qualified Data.Set as Set
+
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = error "You need to implement this function."
+sumOfMultiples items level = sum
+                             $ Set.unions
+                             $ Set.map (energyMultiplies level)
+                             $ Set.fromList items
+
+
+
+energyMultiplies :: Integer -> Integer -> Set.Set Integer
+energyMultiplies _     0    = Set.empty
+energyMultiplies level item = Set.fromList
+                              $ takeWhile (level >)
+                              $ iterate (item +) item
