0dfe4ce0c1b06ea69081ddfdbc9fea5b5d9af5f7
Ilya Shmygol <ishmygol@gmail.com>
2024-12-30 00:25:40 +0100

    Solve exercise atbash-cipher v2

--- atbash-cipher/src/Atbash.hs
+++ atbash-cipher/src/Atbash.hs
@@ -3,30 +3,28 @@ module Atbash (decode, encode) where
 import qualified Data.Map as Map
 import qualified Data.Char as Char
 
+chanks :: Int -> [a] -> [[a]]
+chanks _ [] = []
+chanks n xs = take n xs : chanks n (drop n xs)
+
 mapChar :: Map.Map Char Char -> Char -> Maybe Char
 mapChar dict c | Char.isDigit c = Just c
                | otherwise = Map.lookup c dict
 
-mapStringWith :: Map.Map Char Char -> (String -> Integer -> String) -> String -> String
-mapStringWith dict f = mapStringWith' 0 ""
+mapString :: Map.Map Char Char -> String -> String
+mapString dict = foldr foldingFunction ""
     where
-        mapStringWith' :: Integer -> String -> String -> String
-        mapStringWith' _ acc [] = acc
-        mapStringWith' i acc (x:xs) =
-          case mapChar dict (Char.toLower x) of
-            Nothing -> mapStringWith' i acc xs
-            Just(mappedChar) -> mapStringWith' (i + 1) (acc ++ (f [mappedChar] i)) xs
-
+        foldingFunction :: Char -> String -> String
+        foldingFunction c acc = case mapChar dict $ Char.toLower c of
+            Nothing -> acc
+            Just(mappedChar) -> mappedChar : acc
 
 decode :: String -> String
-decode = mapStringWith decodingDict const
+decode = mapString decodingDict
     where
         decodingDict = Map.fromList $ zip ['z','y'..'a'] ['a'..'z']
 
 encode :: String -> String
-encode = mapStringWith encodingDict prependSeparator
+encode = unwords . chanks 5 . mapString encodingDict
     where
         encodingDict = Map.fromList $ zip ['a'..'z'] ['z','y'..'a']
-        prependSeparator :: String -> Integer -> String
-        prependSeparator s i | i > 0 && i `mod` 5 == 0 = " " <> s
-                             | otherwise = s
