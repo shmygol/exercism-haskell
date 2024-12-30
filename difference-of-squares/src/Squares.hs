cccf69a40fba3f0d5cc825b2d25197a63ff6a1ce
Ilya Shmygol <ishmygol@gmail.com>
2024-12-30 13:03:14 +0100

    Solve exercise difference-of-squares v1

--- difference-of-squares/src/Squares.hs
+++ difference-of-squares/src/Squares.hs
@@ -1,10 +1,14 @@
 module Squares (difference, squareOfSum, sumOfSquares) where
 
 difference :: Integral a => a -> a
-difference n = error "You need to implement this function."
+difference n = (squareOfSum n) - (sumOfSquares n)
 
 squareOfSum :: Integral a => a -> a
-squareOfSum n = error "You need to implement this function."
+squareOfSum n = round $ (^ 2) $ (n' / 2) * (n' + 1)
+    where
+        n' = fromIntegral n
 
 sumOfSquares :: Integral a => a -> a
-sumOfSquares n = error "You need to implement this function."
+sumOfSquares n = round $ (n' ^ 3) / 3.0 + (n' ^ 2) / 2.0 + n' / 6.0
+    where
+        n' = fromIntegral n
