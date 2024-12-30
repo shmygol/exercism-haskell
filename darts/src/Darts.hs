15c151d38013ef88d3eb10b47b0dc5bec90ead67
Ilya Shmygol <ishmygol@gmail.com>
2024-12-30 17:21:45 +0100

    Solve exercise darts v1

--- darts/src/Darts.hs
+++ darts/src/Darts.hs
@@ -1,4 +1,9 @@
 module Darts (score) where
 
 score :: Float -> Float -> Int
-score x y = error "You need to implement this function."
+score x y | distance <= 1 = 10
+          | distance <= 5 = 5
+          | distance <= 10 = 1
+          | otherwise = 0
+    where distance = sqrt (x^2 + y^2)
+
