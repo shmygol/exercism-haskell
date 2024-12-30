ebe867c9a688fed6d3972c3d5f2b5ae57ca1562b
Ilya Shmygol <ishmygol@gmail.com>
2024-12-31 00:48:43 +0100

    Solve exercise space-age v1

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -10,4 +10,11 @@ data Planet = Mercury
             | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = error "You need to implement this function."
+ageOn Mercury seconds = seconds / 7600543.81992
+ageOn Venus seconds = seconds / 19414149.052176
+ageOn Earth seconds = seconds / 31557600.0
+ageOn Mars seconds = seconds / 59354032.69008
+ageOn Jupiter seconds = seconds / 374355659.124
+ageOn Saturn seconds = seconds / 929292362.8848
+ageOn Uranus seconds = seconds / 2651370019.3296
+ageOn Neptune seconds = seconds / 5200418560.032
