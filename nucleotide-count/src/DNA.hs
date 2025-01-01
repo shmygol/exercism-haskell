c67aa9d97e8015ce99bbd2a6cf06319df9e6eb72
Ilya Shmygol <ishmygol@gmail.com>
2025-01-01 15:06:02 +0100

    Solve exercise nucleotide-count v1

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -1,8 +1,20 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import Data.Map (Map)
+import Control.Monad (foldM)
+import qualified Data.Map as Map
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
+fromChar :: Char -> Either String Nucleotide
+fromChar 'A' = Right A
+fromChar 'C' = Right C
+fromChar 'G' = Right G
+fromChar 'T' = Right T
+fromChar _ = Left "Invalid nucleotide"
+
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts = foldM count Map.empty . map fromChar
+  where
+    count acc (Right n) = Right $ Map.insertWith (+) n 1 acc
+    count _ (Left e) = Left e
