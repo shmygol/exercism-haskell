1cef2c8617d039260dc21725fb7b17c63c6b7870
Ilya Shmygol <ishmygol@gmail.com>
2025-01-02 18:56:38 +0100

    Solve exercise scrabble-score v1

--- scrabble-score/src/Scrabble.hs
+++ scrabble-score/src/Scrabble.hs
@@ -1,7 +1,18 @@
 module Scrabble (scoreLetter, scoreWord) where
 
+import Data.Char (toUpper)
+
 scoreLetter :: Char -> Integer
-scoreLetter letter = error "You need to implement this function."
+scoreLetter letter | elem normalizedLetter "AEIOULNRST" = 1
+                   | elem normalizedLetter "DG" = 2
+                   | elem normalizedLetter "BCMP" = 3
+                   | elem normalizedLetter "FHVWY" = 4
+                   | elem normalizedLetter "K" = 5
+                   | elem normalizedLetter "JX" = 8
+                   | elem normalizedLetter "QZ" = 10
+                   | otherwise = 0
+    where
+        normalizedLetter = toUpper letter
 
 scoreWord :: String -> Integer
-scoreWord word = error "You need to implement this function."
+scoreWord = sum . map scoreLetter . map toUpper
