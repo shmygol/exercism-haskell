e837abd1f125f6adcbbc9ef82cf1286e9c40952e
Ilya Shmygol <ishmygol@gmail.com>
2025-01-02 16:23:09 +0100

    Solve exercise phone-number v1

--- phone-number/src/Phone.hs
+++ phone-number/src/Phone.hs
@@ -1,4 +1,15 @@
 module Phone (number) where
 
+import Data.Char (isDigit)
+
 number :: String -> Maybe String
-number xs = error "You need to implement this function."
+number rawInput = case clean of
+    xs@(x1:_:_:x4:_:_:_:_:_:_:rest) | rest == [] && isValidLead x1 && isValidLead x4 -> Just xs
+    _ -> Nothing
+
+    where
+        clean = case filter isDigit rawInput of
+            ('1':xs) -> xs
+            xs -> xs
+
+        isValidLead = flip elem ['2'..'9']
