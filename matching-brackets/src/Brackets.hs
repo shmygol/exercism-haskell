f9bb288ed73dae778a32aa9aa92d09def279e859
Ilya Shmygol <ishmygol@gmail.com>
2025-04-27 00:26:42 +0200

    Solve exercise matching-brackets v1

--- matching-brackets/src/Brackets.hs
+++ matching-brackets/src/Brackets.hs
@@ -1,4 +1,24 @@
 module Brackets (arePaired) where
 
+import qualified Data.Map as Map
+import Data.Maybe()
+import Control.Monad(foldM)
+
+brackets :: Map.Map Char Char
+brackets = Map.fromList [('(', ')'), ('[', ']'), ('{', '}')]
+
+brackets' :: Map.Map Char Char
+brackets' = Map.foldrWithKey (\k v m -> Map.insert v k m) Map.empty brackets
+
 arePaired :: String -> Bool
-arePaired xs = error "You need to implement this function."
+arePaired = (Just [] == ) . foldM f []
+  where
+    f stack x
+      | Map.member x brackets = Just (x:stack)
+      | (Just matchingOpenBracket) <- Map.lookup x brackets'
+        = case stack of
+            [] -> Nothing
+            (prev:stackTail) -> if prev == matchingOpenBracket
+                                then Just stackTail
+                                else Nothing
+      | otherwise = Just stack
